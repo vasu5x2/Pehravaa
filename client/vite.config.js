0d66f47ff6a28a9d3d5e9b81c1d357581c832571
vasu5x2 <vasu40002@gmail.com>
2025-08-06 10:39:27 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist', 
+  },
+  base: '/',
 })
